b37b3b204fe721a5f9c7cd8e9aef549d718af018
Joseph Sabo <jsephsabo@gmail.com>
2025-05-14 11:46:42 -0600

    Week 3 complete

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,11 +1,12 @@
-export default function Item({name, quantity, category}) {
+export default function Item({ name, quantity, category }) {
   return (
-    <li className="p-2 m-4 bg-slate-900 max-w-sm">
-
-        name: {name}
-        quantity: {quantity}
-        category: {category}
-
+    <li
+      className="p-4 m-4 max-w-sm font-bold text-zinc-950 rounded-lg shadow"
+      style={{ backgroundColor: '#CED46A' }}
+    >
+      <div className="text-2xl">name: {name}</div>
+      <div>quantity: {quantity}</div>
+      <div>category: {category}</div>
     </li>
   );
 }
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,8 +2,8 @@ import ItemList from "./item-list";
 
 export default function WeekThreePage() {
     return (
-        <main>
-            <h1>Shopping List</h1>
+        <main className="bg-[#07553B] min-h-screen p-4">
+            <h1 className="text-3xl font-bold mb-4 ml-25 underline">Shopping List</h1>
             <ItemList />
         </main>
     );

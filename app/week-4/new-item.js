14602eddbe255f8336ca6c0d9d2781a361d482a5
Joseph Sabo <jsephsabo@gmail.com>
2025-05-27 10:27:54 -0600

    Finished Week 4 Assignment

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -13,37 +13,50 @@ export default function NewItem() {
   function decrement() {
     setQuantity(prev => (prev > 1 ? prev - 1 : prev));
   }
+  function reset() {
+    setQuantity(1)
+  }
 
   return (
     <div className="p-6 bg-white rounded-lg shadow-md text-center">
       <h2 className="text-xl font-semibold mb-4 text-black">Set Quantity</h2>
-      <div className="flex items-center justify-center space-x-4 text-black">
-        
-        {/* Button to lower the value of the quantity variable - going under 1 not allowed*/}
-        <button
-          onClick={decrement}
-          disabled={quantity === 1}
-          className={`px-4 py-2 text-white rounded ${
-            quantity === 1 ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600'
-          }`}
-        >
-          -
-        </button>
 
-        <span className="text-lg font-bold">{quantity}</span>
-        
-        {/* Button to raise the value of the quantity variable - going above 20 not allowed */}
-        <button
-          onClick={increment}
-          disabled={quantity === 20}
-          className={`px-4 py-2 text-white rounded ${
-            quantity === 20 ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600'
-          }`}
-        >
-          +
-        </button>
-        
+        <div className="text-black text-2xl font-bold mb-4">{quantity}</div>
+
+        <div className="flex justify-center space-x-4">
+          {/* Button to lower the value of the quantity variable - going under 1 not allowed*/}
+          <button
+            onClick={decrement}
+            disabled={quantity === 1}
+            className={`w-10 px-4 py-2 text-white rounded ${
+              quantity === 1 ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600'
+            }`}
+          >
+            -
+          </button>
+          
+          {/* Button to raise the value of the quantity variable - going above 20 not allowed */}
+          <button
+            onClick={increment}
+            disabled={quantity === 20}
+            className={`w-10 px-4 py-2 text-white rounded ${
+              quantity === 20 ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600'
+            }`}
+          >
+            +
+          </button>
+
+          {/* Button to reset the value of the quanitity variable - going back to 1 */}
+          <button
+            onClick={reset}
+            disabled={quantity === 1}
+            className={`w-10 px-4 py-2 text-white rounded ${
+              quantity === 1 ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600'
+            }`}
+          >
+            ↻
+          </button>
+        </div>
       </div>
-    </div>
   );
 }
